5b879d0729bbc962e8db4ed5e194514efab32227
Daniel Vlasceanu <danielvlasceanu@gmail.com>
2012-06-20 14:49:16 +0300

    Added Login interface
    The Login interface is not yet functional. Added LogHandler.dll. Added
    Database connection test code. Renamed the DataContext.

--- AppDevTracker/MainWindow.xaml.cs
+++ AppDevTracker/MainWindow.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+// Custom namespaces
+using AppDevTracker.DataContext;
+using Log = Common.Diagnostics.LogHandler;
 
 namespace AppDevTracker
 {
@@ -22,6 +25,73 @@ namespace AppDevTracker
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            bool available = this.GetDatabaseConnection();
+
+            // Configure according connectivity status
+            this.SetLoginStatus(available);
+            this.SetConnectionStatus(available);
+
+            // Save connection status
+            Properties.Settings.Default.Connected = available;
+            Properties.Settings.Default.Save();
+        }
+
+        private bool GetDatabaseConnection()
+        {
+            using (System.Data.SqlClient.SqlConnection connection = new System.Data.SqlClient.SqlConnection(Properties.Settings.Default.ADTConnectionString))
+            {
+                if (connection.State != System.Data.ConnectionState.Closed)
+                    connection.Close();
+
+                try
+                {
+                    connection.Open();
+
+                    return true;
+                }
+                catch (InvalidOperationException ex)
+                {
+                    // Unspecified datasource or the connection is already open.
+                    Properties.Settings.Default.Connected = false;
+                    MessageBox.Show("Database connection failed. Contact the application administrator.");
+
+                    return false;
+                }
+                catch (System.Data.SqlClient.SqlException ex)
+                {
+                    // Other Sql internal error or expired sql password.
+                    Properties.Settings.Default.Connected = false;
+                    MessageBox.Show("Database connection failed. This kind of error usually arises when the authentication password expires\r\nbut can also be caused by other SQL Server internal problems.\r\n\r\nPlease contact the application administrator.");
+
+                    return false;
+                }
+                finally
+                {
+                    if (connection.State != System.Data.ConnectionState.Closed)
+                        connection.Close();
+                }
+            }
+        }
+        private void SetLoginStatus(bool status)
+        {
+            this.spLogin.IsEnabled = status;
+        }
+        private void SetConnectionStatus(bool status)
+        {
+            if (status == true)
+            {
+                tblConnectionStatus.Text = "The database is available and ready.";
+                bdrFooter.Background = Brushes.PowderBlue;
+            }
+            else
+            {
+                tblConnectionStatus.Text = "The database is NOT available.";
+                bdrFooter.Background = Brushes.MistyRose;
+            }
         }
     }
 }
--- AppDevTracker/Properties/Settings.Designer.cs
+++ AppDevTracker/Properties/Settings.Designer.cs
@@ -8,22 +8,52 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AppDevTracker.Properties
-{
-
-
+namespace AppDevTracker.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "10.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(local);Initial Catalog=AppTrackDB;User ID=appdevuser;Password=1234")]
+        public string ADTConnectionString {
+            get {
+                return ((string)(this["ADTConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Connected {
+            get {
+                return ((bool)(this["Connected"]));
+            }
+            set {
+                this["Connected"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool LoggedIn {
+            get {
+                return ((bool)(this["LoggedIn"]));
+            }
+            set {
+                this["LoggedIn"] = value;
             }
         }
     }
